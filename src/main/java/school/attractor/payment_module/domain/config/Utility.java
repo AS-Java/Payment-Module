43de2204b455e541cf28837d7df52406ba0f0176
Art <th1s1sartakk@gmail.com>
2020-06-11 17:21:32 +0600

    #65 Вынес метод Mask для маскирования данных в отдельный класс Utility

---
+++ src/main/java/school/attractor/payment_module/domain/config/Utility.java
@@ -0,0 +1,45 @@
+package school.attractor.payment_module.domain.config;
+
+public class Utility {
+
+    public static String maskCardNumber(long cardNumber, String mask) {
+        String cardNumberString = String.valueOf(cardNumber);
+        int index = 0;
+
+        StringBuilder maskedNumber = new StringBuilder();
+        for (int i = 0; i < mask.length(); i++) {
+            char c = mask.charAt(i);
+            if (c == '#') {
+                maskedNumber.append(cardNumberString.charAt(index));
+                index++;
+            } else if (c == 'x') {
+                maskedNumber.append(c);
+                index++;
+            } else {
+                maskedNumber.append(c);
+            }
+        }
+        return maskedNumber.toString();
+    }
+
+
+    public static String maskCardNumber(String cardNumber, String mask) {
+
+        int index = 0;
+
+        StringBuilder maskedNumber = new StringBuilder();
+        for (int i = 0; i < mask.length(); i++) {
+            char c = mask.charAt(i);
+            if (c == '#') {
+                maskedNumber.append(cardNumber.charAt(index));
+                index++;
+            } else if (c == 'x') {
+                maskedNumber.append(c);
+                index++;
+            } else {
+                maskedNumber.append(c);
+            }
+        }
+        return maskedNumber.toString();
+    }
+}
